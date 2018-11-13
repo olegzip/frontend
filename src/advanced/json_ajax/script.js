9d01d4012680deeadc085c1b96aea44889eeddd2
olegzip <Gibras90>
2018-11-13 07:26:56 +0200

    AdvHW2 added footer img

---
+++ src/advanced/json_ajax/script.js
@@ -0,0 +1,4 @@
+let jsonData = '{"users": [{"firstname": "username1","lastname": "lastname1"},
+{"firstname":"username2","lastname":"lastname2"},
+{"firstname":"username3","lastname":"lastname3"}
+]}';
