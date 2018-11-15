1e31100ee1e1269dbffd4763372b266be6f64b2e
olegzip <olegzip@ukr.net>
2018-11-16 00:25:02 +0200

    JS homework 16 minor fixes (classnames)

--- src/homework2/script.js
+++ src/homework2/script.js
@@ -1,9 +1,11 @@
 document.getElementById('tumbler').addEventListener("click", function() {
     let darkTheme = document.documentElement.classList.toggle('dark-theme');
-    document.getElementById('tumbler').classList.toggle('enabled');
+    document.getElementById('tumbler').classList.toggle('dark-theme-enabled');
+    // console.log(darkTheme);
     localStorage.setItem('darkTheme', darkTheme);
 });
 
 if (JSON.parse(localStorage.getItem('darkTheme'))) {
     document.documentElement.classList.add('dark-theme');
+    document.getElementById('tumbler').classList.add('dark-theme-enabled');
 }
